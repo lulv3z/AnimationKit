7099be7bf73292f12b112433491595edc7729da1
lulv3z <collincollin.sommer@gmail.com>
2023-06-10 01:49:00 +0200

    SlideInHorizontal and BlendIn Animation Added

--- AnimationKit/BlendInAnimation.cs
+++ AnimationKit/BlendInAnimation.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
 using System.Windows.Forms;
-using System.Drawing;
 
 namespace AnimationKit
 {
-    public class BlendOutAnimation
+    public class BlendInAnimation
     {
         private Timer timer;
         private Form form;
@@ -18,11 +16,11 @@ namespace AnimationKit
         private int currentStep;
         private double opacityStep;
 
-        public BlendOutAnimation(Form form, int duration, int steps)
+        public BlendInAnimation(Form form, int duration, int steps)
         {
-            this.form = form; // Store the form on which the blend-out effect will be applied
-            this.duration = duration; // Store the total duration of the blend-out effect in milliseconds
-            this.steps = steps; // Store the number of steps to achieve the blend-out effect
+            this.form = form; // Store the form on which the blend-in effect will be applied
+            this.duration = duration; // Store the total duration of the blend-in effect in milliseconds
+            this.steps = steps; // Store the number of steps to achieve the blend-in effect
 
             timer = new Timer(); // Create a timer to control the animation
             timer.Tick += Timer_Tick; // Add the event handler for the timer's Tick event
@@ -34,8 +32,10 @@ namespace AnimationKit
             int interval = duration / steps; // Calculate the time interval between steps
             opacityStep = 1.0 / steps; // Calculate the step change for the form's Opacity property
 
+            form.Visible = true; // Set the visibility of the form to true
+
             timer.Interval = interval; // Set the timer's interval
-            timer.Start(); // Start the timer to begin the blend-out effect
+            timer.Start(); // Start the timer to begin the blend-in effect
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -43,15 +43,14 @@ namespace AnimationKit
             if (currentStep >= steps)
             {
                 timer.Stop(); // Stop the timer when all steps are completed
-                form.Visible = false; // Set the visibility of the form to false to hide it
             }
             else
             {
-                double opacity = form.Opacity - opacityStep; // Calculate the new opacity based on the current step
-                form.Opacity = opacity; // Set the form's Opacity property to the new value
+                double opacity = form.Opacity + opacityStep; // Calculate the new opacity based on the current step
+                form.Opacity = Math.Min(opacity, 1.0); // Set the form's Opacity property to the new value, ensuring it doesn't exceed 1.0
+
                 currentStep++; // Increment the current step
             }
         }
-
     }
 }
--- AnimationKit/SlideInHorizontalAnimation.cs
+++ AnimationKit/SlideInHorizontalAnimation.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
 using System.Windows.Forms;
-using System.Drawing;
 
 namespace AnimationKit
 {
-    public class BlendOutAnimation
+    public class SlideInHorizontalAnimation
     {
         private Timer timer;
         private Form form;
         private int duration;
         private int steps;
         private int currentStep;
-        private double opacityStep;
+        private int slideDistance;
+        private int slideStep;
 
-        public BlendOutAnimation(Form form, int duration, int steps)
+        public SlideInHorizontalAnimation(Form form, int duration, int steps, int slideDistance)
         {
-            this.form = form; // Store the form on which the blend-out effect will be applied
-            this.duration = duration; // Store the total duration of the blend-out effect in milliseconds
-            this.steps = steps; // Store the number of steps to achieve the blend-out effect
+            this.form = form; // Store the form on which the slide effect will be applied
+            this.duration = duration; // Store the total duration of the slide effect in milliseconds
+            this.steps = steps; // Store the number of steps to achieve the slide effect
+            this.slideDistance = slideDistance; // Store the total distance to slide the form
 
             timer = new Timer(); // Create a timer to control the animation
             timer.Tick += Timer_Tick; // Add the event handler for the timer's Tick event
@@ -32,10 +32,10 @@ namespace AnimationKit
         {
             currentStep = 0; // Set the current step to 0
             int interval = duration / steps; // Calculate the time interval between steps
-            opacityStep = 1.0 / steps; // Calculate the step change for the form's Opacity property
+            slideStep = slideDistance / steps; // Calculate the step distance to slide the form
 
             timer.Interval = interval; // Set the timer's interval
-            timer.Start(); // Start the timer to begin the blend-out effect
+            timer.Start(); // Start the timer to begin the slide effect
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -43,15 +43,14 @@ namespace AnimationKit
             if (currentStep >= steps)
             {
                 timer.Stop(); // Stop the timer when all steps are completed
-                form.Visible = false; // Set the visibility of the form to false to hide it
             }
             else
             {
-                double opacity = form.Opacity - opacityStep; // Calculate the new opacity based on the current step
-                form.Opacity = opacity; // Set the form's Opacity property to the new value
+                int newX = form.Location.X + slideStep; // Calculate the new X coordinate based on the current step
+                form.Location = new System.Drawing.Point(newX, form.Location.Y); // Set the form's new location
+
                 currentStep++; // Increment the current step
             }
         }
-
     }
 }
